10b5e1c2a82b70f3151abced2cb612662361d811
Alec Hyde <alechyde91@gmail.com>
2018-08-31 15:03:52 -0700

    queried requests are returning from the server

--- challenge_1/client/app.jsx
+++ challenge_1/client/app.jsx
@@ -1,16 +1,53 @@
 import React from 'react';
+import ReactPaginate from 'react-paginate';
+import $ from 'jquery';
+
 
 class App extends React.Component {
   constructor(props) {
     super(props)
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+    this.sendRequestToJSONServer = this.sendRequestToJSONServer.bind(this);
+    this.state = {
+      searchText: '',
+      searchResults: []
+    }
+
+  }
+
+  handleChange(e) {
+    this.setState({
+      searchText: e.target.value
+    }, () => console.log('STATE', this.state.searchText))
+
+  }
+
+  handleClick() {
+    console.log('clicked')
+    this.sendRequestToJSONServer(this.state.searchText)
+  }
+
+  sendRequestToJSONServer(textToSearch) {
+    console.log('sendRequest', textToSearch)
+    $.ajax({
+      url: `http://localhost:3000/events?q=${textToSearch}`,
+      type: 'GET',
+      success: (data) => console.log('SUCCESS \n', data),
+      error: (error) => console.log('ERROR \n', error)
+
+    })
 
   }
 
   render() {
     return(
       <div>
-        <input />
-        <div>Something is on the page</div>
+        <span>Enter text here to search for historical events containing your keyword </span>
+        <input type="text" onChange={this.handleChange}/>
+        <button onClick={this.handleClick}>SEARCH</button>
+        <div>{this.state.searchResults}</div>
       </div>
     )
   }
