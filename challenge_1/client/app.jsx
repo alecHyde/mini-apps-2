a104d20614283824496294db99a2f1883aafa4d6
Alec Hyde <alechyde91@gmail.com>
2018-08-31 11:38:30 -0700

    live server is working and serving my static files

--- challenge_1/client/app.jsx
+++ challenge_1/client/app.jsx
@@ -1,2 +1,25 @@
+import React from 'react';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+
+  }
+
+  render() {
+    return(
+      <div>
+        <input />
+        <div>Something is on the page</div>
+      </div>
+    )
+  }
+}
+
+export default App;
 
 
+// Build a React UI that allows the user to search for historical events based on a keyword. 
+// Use the full-text search features of json-server to return a result to the UI for browsing. 
+// Paginate the list of events using react-paginate, loading no more than ten at a time. 
+// Ensure you are implementing server-side pagination NOT client-side pagination.
